ba9301474d56c5d7bcdb256e3b697e7702420cc9
Sathi Surya Suhas Reddy <83052646+SuhasReddy651@users.noreply.github.com>
2025-03-11 22:28:57 +0000

    Made Changes to API

--- chitchat_module.py
+++ chitchat_module.py
@@ -25,7 +25,7 @@ class ChitChatSystem:
 
         # Call the Gemini API
         response = self.client.models.generate_content(
-            model="gemini-2.0-flash-lite", contents=f"Respond based on the messages if needed, or else just respond as usual. Keep every response short, sweet and concise. Here are the messages: {messages}"
+            model="gemini-2.0-flash-lite", contents=f"Respond based on the previous messages if the user asks for something in extension to the previous messages, or else just respond as usual. Keep every response short, sweet and concise. Here are the messages: {messages}"
         )
 
         # Extract and return the response content
